243fe743f033277159c5bec077a03213c0c9a041
AdamuWanganya <wanganyaadamu@gmail.com>
2022-01-16 22:42:23 +0300

    method to delete location by id

--- src/main/java/Locations.java
+++ src/main/java/Locations.java
@@ -43,4 +43,13 @@ public class Locations {
                     .getKey();
         }
     }
+
+    public void delete(){
+        try (Connection con=DB.sql2o.open()){
+            String sql="DELETE FROM locations WHERE id=:id";
+            con.createQuery(sql)
+                    .addParameter("id",this.id)
+                    .executeUpdate();
+        }
+    }
 }
