e4611da79a723b7bb33ec879ce6830a1e24d7503
AdamuWanganya <wanganyaadamu@gmail.com>
2022-01-16 22:25:05 +0300

    test to return endangered animal by id

--- src/test/java/EndangeredAnimalsTest.java
+++ src/test/java/EndangeredAnimalsTest.java
@@ -22,4 +22,13 @@ public class EndangeredAnimalsTest {
         testAnimal.save();
         assertTrue(EndangeredAnimals.all().get(0).getHealth().equals(testAnimal.getHealth()));
     }
+
+    @Test
+    public void findByIdReturnsCorrectInfo(){
+        EndangeredAnimals testAnimal=setUpNewAnimal();
+        testAnimal.save();
+        Animals foundAnimal= Animals.find(testAnimal.getId());
+        assertTrue(foundAnimal.getHealth().equals(testAnimal.getHealth()));
+
+    }
 }
