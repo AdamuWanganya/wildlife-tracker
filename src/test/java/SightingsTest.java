723850047a8ed8cf0e146bed547335b47ce387e2
AdamuWanganya <wanganyaadamu@gmail.com>
2022-01-16 23:05:44 +0300

    test for all instances are saved

--- src/test/java/SightingsTest.java
+++ src/test/java/SightingsTest.java
@@ -14,4 +14,18 @@ public class SightingsTest {
         Sightings sighting = setUpNewSighting();
         assertEquals(true, sighting instanceof Sightings);
     }
+
+    @Test
+    public void allInstancesAreSaved() {
+        Sightings sightings=setUpNewSighting();
+        Sightings otherSighting=new Sightings(-1,1,1);
+        try {
+            sightings.save();
+            otherSighting.save();
+            assertTrue(Sightings.find(sightings.getId()).equals(sightings));
+        }catch (IllegalArgumentException e){
+            System.out.println(e);
+        }
+    }
 }
+
