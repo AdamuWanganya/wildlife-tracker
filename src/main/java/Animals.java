db1de47f37af6fec0c87cc228fae1bd65c230dd7
AdamuWanganya <wanganyaadamu@gmail.com>
2022-01-16 21:57:22 +0300

    declare and assigning values in animal class

--- src/main/java/Animals.java
+++ src/main/java/Animals.java
@@ -1,2 +1,18 @@
-public class Animals {
+public class Animals implements DatabaseManagement {
+
+    public int id;
+    public String name;
+    public String type;
+    private String health;
+    private String age;
+    public static final String ANIMAL_TYPE="normal";
+
+
+    public Animals(String name,String type) {
+        this.name = name;
+        this.type=ANIMAL_TYPE;
+        this.health="";
+        this.age="";
+    }
+
 }
