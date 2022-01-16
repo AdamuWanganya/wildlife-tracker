58ffc68167ff0090c06b75de2fad93427dd0cae3
AdamuWanganya <wanganyaadamu@gmail.com>
2022-01-16 22:20:46 +0300

    declare value of endangered animals and asigning them values

--- src/main/java/EndangeredAnimals.java
+++ src/main/java/EndangeredAnimals.java
@@ -1,2 +1,21 @@
-public class EndangeredAnimals {
+public class EndangeredAnimals extends Animals implements DatabaseManagement{
+
+    private String health;
+    private String age;
+    public static final String HEALTH_HEALTHY="healthy";
+    public static final String HEALTH_ILL="ill";
+    public static final String HEALTH_OKAY="okay";
+
+    public static final String AGE_NEWBORN="newborn";
+    public static final String AGE_YOUNG="young";
+    public static final String AGE_ADULT="adult";
+
+    public static final String ANIMAL_TYPE="endangered";
+    public EndangeredAnimals(String name,String type,String health,String age) {
+        super(name,type);
+        this.type=type;
+        this.health=health;
+        this.age=age;
+
+    }
 }
