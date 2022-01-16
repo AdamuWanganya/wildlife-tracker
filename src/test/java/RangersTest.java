9a89781f3e4e3e198bb591d30e1bfd7f978a9a18
AdamuWanganya <wanganyaadamu@gmail.com>
2022-01-16 22:49:39 +0300

    instantiates rangers test

--- src/test/java/RangersTest.java
+++ src/test/java/RangersTest.java
@@ -1,5 +1,20 @@
 import junit.framework.TestCase;
+import org.junit.Rule;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+
 
 public class RangersTest extends TestCase {
 
+
+    @Rule
+    public DatabaseRule databaseRule = new DatabaseRule();
+
+    @Test
+    public void createInstanceOfRangersClass_true() {
+        Rangers ranger = setUpNewRanger();
+        assertEquals(true, ranger instanceof Rangers);
+    }
 }
