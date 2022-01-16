8d09f6925a35e3bf8d5aa417a9a7d886cc987ecf
AdamuWanganya <wanganyaadamu@gmail.com>
2022-01-16 22:22:15 +0300

    instantiate value of endangered animal

--- src/test/java/EndangeredAnimalsTest.java
+++ src/test/java/EndangeredAnimalsTest.java
@@ -1,5 +1,18 @@
-import junit.framework.TestCase;
+import org.junit.Rule;
+import org.junit.Test;
 
-public class EndangeredAnimalsTest extends TestCase {
+import java.util.List;
 
+import static org.junit.Assert.*;
+
+public class EndangeredAnimalsTest {
+
+    @Rule
+    public DatabaseRule databaseRule = new DatabaseRule();
+
+    @Test
+    public void testInstanceOfEndangeredAnimalsClass_true() {
+        EndangeredAnimals testAnimal = setUpNewAnimal();
+        assertEquals(true, testAnimal instanceof EndangeredAnimals);
+    }
 }
